59f6d8ca35d58a42d87fa177a0692cf79d1d3db2
Sumit262601 <skr.04088s@gmail.com>
2025-05-08 17:41:36 +0530

    Update files

--- app.py
+++ app.py
@@ -68,4 +68,5 @@ def progress():
     return jsonify(progress_data)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
