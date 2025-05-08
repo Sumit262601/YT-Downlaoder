b4c16641bc6e4ccda408e752398559cda8e92886
Sumit262601 <skr.04088s@gmail.com>
2025-05-08 18:26:08 +0530

    Add render.yaml and gunicorn.conf.py

--- app.py
+++ app.py
@@ -68,5 +68,4 @@ def progress():
     return jsonify(progress_data)
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', debug=True)
