3a5b0322fdb1c30e1498ddb8dff0eb80e74e8165
Sumit262601 <skr.04088s@gmail.com>
2025-05-10 11:31:28 +0530

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,16 +1,10 @@
 @echo off
 echo Installing YouTube Downloader dependencies...
 
-REM Create and activate virtual environment
 python -m venv venv
 call venv\Scripts\activate.bat
-
-REM Update pip and yt-dlp
 python -m pip install --upgrade pip
-python -m pip install --upgrade yt-dlp --force-reinstall
-
-REM Install other requirements
-python -m pip install customtkinter pillow certifi
+python -m pip install -r requirements.txt
 
 echo Installation completed!
 pause
